4b5db9a883380fba0aeb2f3c30ada163ac55426b
SupremeSoviet <vanishedgrace@list.ru>
2024-06-14 20:33:43 +0500

    Update web app

--- web/app.py
+++ web/app.py
@@ -113,7 +113,7 @@ def query_to_dataframe(result):
     return df[['source', 'url', 'title', 'time', 'text']]  # Select only required columns
 
 
-def get_top_df(text):
+def get_top_df(text, ans):
     embedding = get_embedding(text)
     query = f"""
     SELECT
@@ -132,9 +132,11 @@ def get_top_df(text):
     try:
         result = execute_query(query)
         df = query_to_dataframe(result)
-        return df
+        return df, ans
     except Exception as ex:
         print('get top df ex: ', ex)
+        ans += 'get top df ex: ' + str(ex)
+        return pd.DataFrame(), ans
 
 
 def get_summary(text: str):
@@ -489,17 +491,18 @@ def send_email_with_attachment(to_email, subject, body, attachment_path):
         print(f'Failed to send email: {e}')
 
 def send_pdf():
+    ans = ''
     try:
-        print(1)
+        ans += '1 '
         descr_file = open('uploads/description.txt', 'r')
         descr = descr_file.read()
         descr_file.close()
+        ans += descr + ' '
+        ans += '2 '
 
-        print(2)
+        df, ans = get_top_df(descr, ans)
 
-        df = get_top_df(descr)
-
-        print(3)
+        ans += '3 '
 
         current_day = datetime.now().day
         current_month = datetime.now().month
@@ -576,10 +579,10 @@ def send_pdf():
             send_email_with_attachment(email, f"Дайджест новостей от {current_day}.{current_month}.{current_year}", email_body, pdf_path)
         print("PDF Sent")
 
-        return True, 1
+        return True, ans
     except Exception as ex:
         print(ex)
-        return False, ex
+        return False, ans
 
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
