0e8a6ee9baa4329f6015c1d7a56d35bfa0969552
SupremeSoviet <vanishedgrace@list.ru>
2024-06-14 13:35:45 +0500

    Update web app

--- web/app.py
+++ web/app.py
@@ -1,15 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_apscheduler import APScheduler
 import dotenv
-import os
 import hashlib
-import pandas as pd
 import re
 import json
-import requests
+import time
 import numpy as np
+import ssl
+import os
+import certifi
+import requests
+import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+from reportlab.platypus import Image
+from datetime import datetime
 
 dotenv.load_dotenv()
+
+pdf_path = 'news_digest.pdf'
+img_path = 'photo.webp'
+style_Arial_path = 'arialmt.ttf'
+SMTP_SERVER = 'smtp.mail.ru'
+SMTP_PORT = 587
+SMTP_USERNAME = os.getenv('MAIL_USER') # Почта, с которой отправляется
+SMTP_PASSWORD = os.getenv('MAIL_PWD') # Пароль почты (для доступа к почте внешним приложениям) с которой отправляется
+Sent_to = []
 
 gpt_api_key = os.getenv('API_KEY')
 FOLDER_ID = os.getenv('FOLDER_ID')
@@ -18,6 +45,88 @@ yagpt3_uri = f'gpt://{FOLDER_ID}/yandexgpt/latest'
 embed_url = "https://llm.api.cloud.yandex.net:443/foundationModels/v1/textEmbedding"
 completion_url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
 headers = {"Content-Type": "application/json", "Authorization": f"Api-Key {gpt_api_key}", "x-folder-id": f"{FOLDER_ID}"}
+
+cert_dir = 'certs'
+clickhouse_host = os.getenv('CLICKHOUSE_HOST')
+clickhouse_user = os.getenv('CLICKHOUSE_USER')
+clickhouse_password = os.getenv('CLICKHOUSE_PASSWORD')
+clickhouse_port = os.getenv('CLICKHOUSE_PORT')
+cert_path = cert_dir + '/RootCA.pem'
+db_name = os.getenv('CLICKHOUSE_DB_NAME')
+table_name = os.getenv('CLICKHOUSE_TABLE_NAME')
+
+def download_certificate(url, save_path):
+    response = requests.get(url)
+    with open(save_path, 'wb') as file:
+        file.write(response.content)
+
+def install_certificates(cert_dir):
+    os.makedirs(cert_dir, exist_ok=True)
+
+    root_ca_url = "https://storage.yandexcloud.net/cloud-certs/RootCA.pem"
+    intermediate_ca_url = "https://storage.yandexcloud.net/cloud-certs/IntermediateCA.pem"
+
+    root_ca_path = os.path.join(cert_dir, "RootCA.pem")
+    intermediate_ca_path = os.path.join(cert_dir, "IntermediateCA.pem")
+
+    download_certificate(root_ca_url, root_ca_path)
+    download_certificate(intermediate_ca_url, intermediate_ca_path)
+
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    ssl_context.load_verify_locations(root_ca_path)
+    ssl_context.load_verify_locations(intermediate_ca_path)
+
+    return ssl_context
+
+install_certificates(cert_dir)
+
+def execute_query(query):
+    response = requests.get(
+        f'https://{clickhouse_host}:{clickhouse_port}',
+        params={
+            'query': query,
+        },
+        headers={
+            'X-ClickHouse-User': f'{clickhouse_user}',
+            'X-ClickHouse-Key': f'{clickhouse_password}',
+        },
+        verify=f'{cert_path}'
+    )
+
+    return response.text
+
+
+def query_to_dataframe(result):
+    # Split the result into lines
+    data = [line.split('\t') for line in result.strip().split('\n')]
+    # Extract headers and rows
+    headers = ['source', 'url', 'title', 'time', 'text', 'vector', 'cosine']
+    rows = data[:]
+    # Create DataFrame
+    df = pd.DataFrame(rows, columns=headers)
+    return df[['source', 'url', 'title', 'time', 'text']]  # Select only required columns
+
+
+def get_top_df(text):
+    embedding = get_embedding(text)
+    query = f"""
+    SELECT
+        source,
+        url,
+        title,
+        time,
+        text,
+        embedding,
+        1 - cosineDistance(embedding, [{",".join(map(str, embedding))}]) AS cosine_similarity
+    FROM news_bd.news_articles
+    WHERE length(embedding) = 256
+    ORDER BY cosine_similarity DESC
+    LIMIT 20
+    """
+    result = execute_query(query)
+    df = query_to_dataframe(result)
+    return df
+
 
 def get_labels(text: str) -> np.array:
 
@@ -137,9 +246,17 @@ def get_embedding(text: str) -> np.array:
     }
 
     try:
+        response = requests.post(embed_url, json=query_data, headers=headers)
+        if 'error' in dict(response.json()).keys():
+            print(response.json()['error'])
+            time.sleep(1)
+        else:
+            return np.array(response.json()['embedding'])
+
         return np.array(
         requests.post(embed_url, json=query_data, headers=headers).json()["embedding"]
         )
+
     except Exception as ex:
         print('embedding ex', ex)
         return np.array([])
@@ -196,7 +313,8 @@ def upload():
         is_correct, ans = check_accs(filename)
         if is_correct:
             flash('Успешно загружено', 'success')
-            mails = ans
+            Sent_to = ans
+            print(Sent_to)
         else:
             flash('Ошибка загрузки: ' + ans, 'error')
     else:
@@ -280,15 +398,115 @@ def check_accs(filename):
 
         if invalid_emails:
             return False, f"Неверный формат почты: {invalid_emails}"
-
-        return True, emails
+        if emails:
+            return True, emails
+        return False, f'Почты не найдены'
     except Exception as e:
         return False, f"Ошибка чтения файла"
 
-def send_pdf():
+def create_pdf(filename, data):
+    doc = SimpleDocTemplate(filename, pagesize=letter)
+    styles = getSampleStyleSheet()
+    styleN = styles['Normal']
+    styleH = styles['Heading1']
+    styleJ = styles['BodyText']
 
-    print("PDF Sent")
-    return False
+    try:
+        pdfmetrics.registerFont(TTFont('Arial', style_Arial_path))
+    except Exception as e:
+        print(f"Ошибка при загрузке шрифта: {e}")
+
+    styleN.fontName = 'Arial'
+    styleH.fontName = 'Arial'
+    styleJ.fontName = 'Arial'
+
+    styleH.fontSize = 20
+    styleJ.fontSize = 12
+
+    styleJ.alignment = 4
+
+    story = []
+
+    for index, row in data.iterrows():
+        story.append(Paragraph(f"{row['title']}", styleH))
+        story.append(Paragraph(f"<font color='green'><u>{row['source']}</u></font>", styleN))
+        story.append(Paragraph(f"Время: {row['time']}", styleN))
+        story.append(Paragraph(f"<font color='blue'><u>{row['url']}</u></font>", styleN))
+        story.append(Paragraph("<br></br>", styleN))
+        story.append(Spacer(1, 12))
+        story.append(Paragraph(f"{row['text']}", styleJ))
+        img = Image(img_path)
+        story.append(img)
+        story.append(Paragraph("<br></br>", styleN))
+
+    doc.build(story)
+
+def send_email_with_attachment(to_email, subject, body, attachment_path):
+    msg = MIMEMultipart()
+    msg['From'] = SMTP_USERNAME
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'html'))
+    print(1)
+    with open(attachment_path, 'rb') as attachment:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename= {os.path.basename(attachment_path)}')
+    msg.attach(part)
+    print(2)
+    try:
+
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(SMTP_USERNAME, SMTP_PASSWORD)
+            server.sendmail(SMTP_USERNAME, to_email, msg.as_string())
+            print(3)
+        print('Email sent successfully')
+    except Exception as e:
+        print(f'Failed to send email: {e}')
+
+def get_summary(txt):
+    pass
+
+
+def send_pdf():
+    try:
+        descr_file = open('description.txt', 'r')
+        descr = descr_file.read()
+        descr_file.close()
+
+        df = get_top_df(descr)
+        current_day = datetime.now().day
+        current_month = datetime.now().month
+        current_year = datetime.now().year
+        if current_day<10: current_day = '0'+str(current_day)
+        if current_month<10: current_month = '0'+str(current_month)
+        filtered_df = df[['source', 'url', 'title', 'time', 'text']]
+        pdf_filename = pdf_path
+        create_pdf(pdf_filename, filtered_df)
+
+
+        html_content = ""
+        for index, row in filtered_df.iterrows():
+            html_content += f"<h2>{row['title']}</h2>"
+            html_content += f"<p><b>Источник:</b> {row['source']}</p>"
+            html_content += f"<p><b>Время:</b> {row['time']}</p>"
+            html_content += f"<p><b>URL:</b> <a href='{row['url']}'>{row['url']}</a></p>"
+            html_content += f"<p><b>Текст:</b> {row['text']}</p>"
+            html_content += "<hr>"
+
+        email_body = f"<h1>Актуальный дайджест новостей:</h1>{html_content}"
+        print(Sent_to)
+        for email in ['van-7890@mail.ru', 'vanishedgrace@list.ru']:
+            print(1)
+            send_email_with_attachment(email, f"Дайджест новостей от {current_day}.{current_month}.{current_year}", email_body, pdf_path)
+
+        print("PDF Sent")
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
 
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
